6b5b874142c004f9969e46a0f7028d3d5dd29a75
dbrudner <dbrudne1@gmail.com>
2018-10-12 15:52:03 -0500

    linter auto --fix changed to actions.js

--- src/features/Issues/actions.js
+++ src/features/Issues/actions.js
@@ -1,59 +1,58 @@
+import axios from 'axios';
 import {
   GET_ISSUES,
   GET_ISSUES_SUCCESS,
   GET_ISSUES_FAILURE,
-  FILTER_ISSUES
+  FILTER_ISSUES,
 } from '../../constants';
 
-import axios from 'axios';
 import config from '../../config';
 
-export const filterIssues = by => {
-  console.log(by)
+export const filterIssues = (by) => {
+  console.log(by);
   return dispatch => dispatch({
     type: FILTER_ISSUES,
     payload: {
-      by: by,
-    }
-  })
-}
-
-
-
-export const getIssues = ({owner, repo, page}) => {
-  const newUrl = `https://api.github.com/repos/${owner}/${repo}/issues?access_token=${config.access_token}&state=all&page=${page}`
-  console.log(newUrl)
-  return dispatch => {
-    dispatch(getIssuesStarted({owner, repo}));
-
-    axios
-      .get(newUrl)
-      .then(res => {
-        dispatch(getIssuesSuccess(res.data));
-      })
-      .catch(err => {
-        dispatch(getIssuesFailure(err.message));
-      });
-  };
+      by,
+    },
+  });
 };
 
 const getIssuesStarted = payload => ({
   type: GET_ISSUES,
   payload: {
     owner: payload.owner,
-    repo: payload.repo
-  }
+    repo: payload.repo,
+  },
 });
 const getIssuesSuccess = issues => ({
   type: GET_ISSUES_SUCCESS,
   payload: {
-    ...issues
-  }
+    ...issues,
+  },
 });
 
 const getIssuesFailure = error => ({
   type: GET_ISSUES_FAILURE,
   payload: {
-    error
-  }
+    error,
+  },
 });
+
+
+export const getIssues = ({ owner, repo, page }) => {
+  const newUrl = `https://api.github.com/repos/${owner}/${repo}/issues?access_token=${config.access_token}&state=all&page=${page}`;
+  console.log(newUrl);
+  return (dispatch) => {
+    dispatch(getIssuesStarted({ owner, repo }));
+
+    axios
+      .get(newUrl)
+      .then((res) => {
+        dispatch(getIssuesSuccess(res.data));
+      })
+      .catch((err) => {
+        dispatch(getIssuesFailure(err.message));
+      });
+  };
+};
