950b5bd8e11e91a0e5355fb3a396ba0dd8dd58fb
Grzegorz Slazak <gregorysl@gmail.com>
2018-10-12 21:06:15 +0200

    Added colors to icons, and move styles to different file

--- src/features/Icon/Icon.js
+++ src/features/Icon/Icon.js
@@ -3,20 +3,30 @@ import Octicon, { getIconByName } from "@githubprimer/octicons-react";
 
 const Icon = ({ state, pull }) => {
   let icon = "";
+  let color = "";
   if (state === "open") {
     if (pull) {
       icon = "git-pull-request";
     } else {
       icon = "issue-opened";
     }
+    color = "green";
   } else {
     if (pull) {
       icon = "git-merge";
+      color = "purple";
     } else {
       icon = "issue-closed";
+      color = "red";
     }
   }
-  return <Octicon className={"card-icon"} icon={getIconByName(icon)} />;
+  return (
+    <Octicon
+      size={"medium"}
+      className={`card-icon ${color}`}
+      icon={getIconByName(icon)}
+    />
+  );
 };
 
 export default Icon;
