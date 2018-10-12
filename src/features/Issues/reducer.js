dd1370f4565dc4929bb3f8d201c1b188aef3da27
dbrudner <dbrudne1@gmail.com>
2018-10-12 15:52:21 -0500

    linter auto --fix changed to reducer.js

--- src/features/Issues/reducer.js
+++ src/features/Issues/reducer.js
@@ -7,48 +7,48 @@ import {
 
 const INITIAL_STATE = {
   issues: {},
-  filteredIssues: {}
-}
+  filteredIssues: {},
+};
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case GET_ISSUES:
+  case GET_ISSUES:
+    return {
+      ...state,
+      fetched: false,
+      isLoading: true,
+    };
+  case GET_ISSUES_SUCCESS:
+    return {
+      ...state,
+      issues: action.payload, // Used to store the original api request data
+      filteredIssues: action.payload, // Actually used in view to handle filtering
+      fetched: true,
+      isLoading: false,
+    };
+  case GET_ISSUES_FAILURE:
+    return {
+      ...state,
+      error: action.payload,
+      fetched: false,
+      isLoading: false,
+    };
+  case FILTER_ISSUES:
+    const by = action.payload.by;
+    if (by === 'all') {
       return {
         ...state,
-        fetched: false,
-        isLoading: true,
+        filterBy: action.payload.by,
+        filteredIssues: state.issues,
       };
-    case GET_ISSUES_SUCCESS:
-      return {
-        ...state,
-        issues: action.payload, // Used to store the original api request data
-        filteredIssues: action.payload, // Actually used in view to handle filtering
-        fetched: true,
-        isLoading: false,
-      };
-    case GET_ISSUES_FAILURE:
-      return {
-        ...state,
-        error: action.payload,
-        fetched: false,
-        isLoading: false
-      };
-    case FILTER_ISSUES:
-      let by = action.payload.by;
-      if (by === "all") {
-        return {
-          ...state,
-          filterBy: action.payload.by,
-          filteredIssues: state.issues
-        }
-      } else {
-        return {
-          ...state,
-          filterBy: action.payload.by,
-          filteredIssues: Object.values(state.issues).filter(issue => issue.state === by)
-        }
-      }
-    default:
-      return state;
+    }
+    return {
+      ...state,
+      filterBy: action.payload.by,
+      filteredIssues: Object.values(state.issues).filter(issue => issue.state === by),
+    };
+
+  default:
+    return state;
   }
 }
