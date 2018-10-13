dea17c2753b10425c3f44b183be4a914c64b982c
Dillion Verma <dillionverma@hotmail.com>
2018-10-12 21:54:25 -0400

    Trblackw master (#35)
    * installed styled components
    
    * created styled li component to render each tag/label color
    
    * erased previous li styles

--- src/features/Issues/Issues.js
+++ src/features/Issues/Issues.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { getIssues, filterIssues } from "./actions";
 import Icon from "./components/Icon/Icon";
+import styled from "styled-components";
 
 const maxLength = 50;
 
@@ -22,7 +23,9 @@ const Card = ({ title, body, labels, issueUrl, owner, repo, number, state,  pull
           <div className="card-labels">
             <ul>
               {labels.map(label => (
-                <li key={label.id}>{label.name}</li>
+               <StyledLi key={label.id} color={label.color}>
+                  {label.name}
+                </StyledLi>
               ))}
             </ul>
           </div>
@@ -179,3 +182,11 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps,
 )(Issues);
+
+
+const StyledLi = styled.li`
+background-color: #${props => props.color};
+color: white;
+padding: .5em;
+border-radius: 5px;
+`;
